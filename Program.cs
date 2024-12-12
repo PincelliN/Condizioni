0eecfd27211c0143398ccec8bf537e9987deb0df
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-12 11:25:44 +0100

    statement if else and switch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string test = "primo";
+
+if (test == "primo")
+    Console.WriteLine("hai scritto primo");
+else if (test == "secondo")
+    Console.WriteLine("hai scritto secondo");
+else
+    Console.WriteLine($"ha scritto {test}");
+
+
+switch (test)
+{
+    case "primo":
+        Console.WriteLine("hai scritto primo switch");
+        break;
+    case "secondo":
+        Console.WriteLine("hai scritto secondo switch");
+        break;
+    default:
+        Console.WriteLine($"ha scritto {test} switch");
+        break;
+}
